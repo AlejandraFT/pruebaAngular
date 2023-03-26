1dde4d88ed5873c76e5a6183225aef2b101d0a1a
AlejandraFT <alejitafranco19999@gmail.com>
2023-03-26 13:51:25 -0500

    creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { MyMessagesComponent } from './views/my-messages/my-messages.component';
+import { AllMessagesComponent } from './views/all-messages/all-messages.component';
+import { CreateMessagesComponent } from './views/create-messages/create-messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component : HomeComponent
+    },
+    {
+        path: 'login',
+        component : LoginComponent
+    },
+    {
+        path: 'register',
+        component : RegisterComponent
+    },
+    {
+        path: 'myMessages',
+        component : MyMessagesComponent
+    },
+    {
+        path: 'allMessages',
+        component : AllMessagesComponent
+    },
+    {
+        path: 'createMessages',
+        component : CreateMessagesComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
