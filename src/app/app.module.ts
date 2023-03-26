95d47290519b29e9363e4a9a699863e6a725b48e
AlejandraFT <alejitafranco19999@gmail.com>
2023-03-26 13:50:57 -0500

    importacion de componentes y parallax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxParallaxModule } from '@yoozly/ngx-parallax';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { AllMessagesComponent } from './views/all-messages/all-messages.component';
+import { MyMessagesComponent } from './views/my-messages/my-messages.component';
+import { CreateMessagesComponent } from './views/create-messages/create-messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    FiltersComponent,
+    MessagesComponent,
+    AllMessagesComponent,
+    MyMessagesComponent,
+    CreateMessagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxParallaxModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
